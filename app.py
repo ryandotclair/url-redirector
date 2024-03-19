01f8d0a5c9af7a417af804a738ca85e02621697b
Ryan Clair <ryan.clair@gmail.com>
2024-03-19 09:19:55 -0500

    init

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ URL=os.environ['URL']
 
 @app.route('/')
 def notHere():
-    return redirect("URL")
+    return redirect(URL)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
